4caec663cb2eb2d69eed0d6a5d92d37fa45b0cca
grind-t <grindt.gd@gmail.com>
2026-06-15 16:07:16 +0300

    fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  name: "@grind-t/russian-bonds",
-  entry: ["lib/index.ts"],
-  outDir: "dist",
-  format: ["esm", "cjs"],
-  experimentalDts: true,
-  clean: true,
+	name: "@grind-t/russian-bonds",
+	entry: ["src/index.ts"],
+	outDir: "dist",
+	format: ["esm", "cjs"],
+	experimentalDts: true,
+	clean: true,
 });
